1c03df97abaacd15b26c33f5258cf21558bb0e3d
sionel <sadb0101@naver.com>
2019-11-26 20:03:18 +0900

    dd

--- src/index.js
+++ src/index.js
@@ -2,11 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Calc from "./Calc";
 
-// const reducer = combineReducers({
-//     calc
-//   });
-
-// const store = configureStore(reducer,)
-
-
 ReactDOM.render(<Calc />, document.getElementById("root"));
